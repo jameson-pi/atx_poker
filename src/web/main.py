026d8f843f45b7b0ca7a3e334ec39ebc431f6ac3
Allen <allen.piscitello@gmail.com>
2021-01-16 10:36:10 -0600

    adding dummy data

--- src/web/main.py
+++ src/web/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from extract import x
-
+from repositories.fixed import TableData
 
 app = Flask(__name__)
 @app.route('/')
 def main_content():
-  return render_template("tables.jinja", y = x)
+  table_data = TableData.get_current_tables()
+  return render_template("tables.jinja", table_data = table_data)
