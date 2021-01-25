58188a0a4a71e25edc96fcdbc4ea9134e413ff64
jameson piscitello <piscitello284@gmail.com>
2021-01-25 21:13:01 +0000

    update test file

--- src/print_data.py
+++ src/print_data.py
@@ -1,3 +1,11 @@
 from repositories.query import TableData
+games = []
 for game in TableData.get_current_tables():
-  print(game)
+  if '\n' in game['table']:
+    game_split = game['table'].split('\n')
+    print('game = ',end = " ")
+    for games in game_split:
+      print(games)
+    print(f'at',game['location'])
+  else:
+    print(f'game =',game['table'],'at',game['location'])
