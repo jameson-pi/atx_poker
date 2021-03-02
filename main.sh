45b40a3711c6294dad0ab95178fc76d66581b002
Jameson Piscitello <piscitello284@gmail.com>
2021-03-02 23:48:55 +0000

    add -p and -i options

--- main.sh
+++ main.sh
@@ -1,4 +1,14 @@
-pip install -r requirements.txt
-cd src                                   
-export FLASK_APP=main.py
-python3 -m flask run             
+#!/bin/sh
+if  [[ $1 = "-i" ]]; then
+    pip install -r requirements.txt
+else
+    pip install -r requirements.txt
+    cd src   
+    if  [[ $1 = "-p" ]]; then
+        python3 print_data.py
+    else
+        export FLASK_APP=main.py
+        python3 -m flask run
+cd ..
+fi
+fi
--- src/print_data.py
+++ src/print_data.py
@@ -3,12 +3,12 @@ games = []
 games = TableData.get_current_tables()
 if games is not None:
   for game in games:
-    if '\n' in game['table']:
-      game_split = game['table'].split('\n')
+    if '\n' in game[2]:
+      game_split = game[2].split('\n')
       print('game = ',end = "")
       for games in game_split:
         print(games)
-      print(f'at',game['location'])
+      print(f'at',game[0])
     else:
-      print(f'game =',game['table'],'at',game['location'])
+      print(f'game =',game[2],'at',game[0])
     print()
